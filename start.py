9343e6d13b84f1ec886a8b29cab4a5e91199e138
Fahrudin Hariadi <fahrudin.hariadi@gmail.com>
2024-12-03 08:22:15 +0700

    -

--- start.py
+++ start.py
@@ -5,9 +5,11 @@ from dotenv import load_dotenv
 import argparse
 from subprocess import Popen
 
-# #######################################################################################################################
+#######################################################################################################################
 if not os.access("config/.gitkeep", os.W_OK):
     os.chmod("/code/config/", 0o0777)
+if not os.access("files/database/db/.gitkeep", os.W_OK):
+    os.chmod("/code/files/database/db/", 0o0777)
 #######################################################################################################################
 # parser = argparse.ArgumentParser(description="Start Applikasi.", epilog="Pilih Module yang mau diJalankan.")
 # parser.add_argument("module", help="Pilih salah satu = ws, celery atau fower")
